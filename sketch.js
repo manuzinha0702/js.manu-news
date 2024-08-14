135840461abd1dd84d2ed15363422ac42dc6f5d5
Manuzinha <alba.santos.manoela@escola.pr.gov.br>
2024-08-14 13:59:13 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,20 @@
+ function setup() {
+  createCanvas(3000, 1000); //cria uma página em branco
+    background("pink");//muda a cor de fundo
+}
+
+function draw() {
+ 
+
+stroke("pink")
+  fill("purple");
+  
+  if(mouseIsPressed) {
+    rect(mouseX,mouseY, 10, 20)
+  }
+  
+
+  
+}
+
+     
